0dc4f58b912b031a32980f0e02a75ef8466ccad6
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-30 10:51:18 -0500

    added second question image

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ var game = {
     alert("Correct!\n+100");
     $(".questionOne").css("display", "none");
     $(".questionTwo").css("display", "block");
-     game.score += 100;
-    $(".score").text(game.score)
+     game.score = game.score + 100;
+    $(".score").html(game.score)
 
   },
+  incorrectAnswer: function(){
+    alert("Sorry, wrong answer.");
+    $(".questionOne").css("display", "none");
+    $(".questionTwo").css("display", "block");
+  }
 }
 
 $(".play").on("click", game.home.bind(game));
 $(".correct").on("click", game.correctAnswer.bind(game));
+$(".incorrect").on("click", game.incorrectAnswer.bind(game));
